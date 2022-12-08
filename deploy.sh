64145ebb1c4f17c29074c3d44e7cd080baa58069
ideyalab <119831306+ideyalab@users.noreply.github.com>
2022-12-08 14:54:36 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#! bin/bash
-sudo ssh -i ${{secrets.PEM_KEY}} ubuntu@ec2-44-206-253-49.compute-1.amazonaws.com
-sudo docker login -u ${{ secrets.DOCKER_USERNAME }} -p ${{ secrets.DOCKER_PASSWD }}
-sudo docker pull ${{ secrets.DOCKER_USERNAME}}/${{secrets.DOCKER_REPO }}
-sudo docker run -it -d -p 8090:8080 citagi63/github
